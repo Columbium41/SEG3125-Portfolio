afad3b67f819d8ab20153b87f25ec1aa1bdecad5
Charley Liu <charleyliu716@gmail.com>
2025-05-16 18:16:27 -0400

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./sections/Navbar"
+import Hero from "./sections/Hero";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
+      <Hero />
     </div>
   )
 }
